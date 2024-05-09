f2bc0d53d498cdb7f57a361113b197b86367908b
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-05-09 18:04:32 -0400

    Simplify unzip script

--- stages/01_unzip.py
+++ stages/01_unzip.py
@@ -23,35 +23,29 @@ def create_zip_re_pattern():
     with open("version.yaml", "r") as f:
         config = yaml.load(f, yaml.Loader)
     VERSION_RE = re.escape(config['version'])
-    ZIP_DIR_PATTERN = re.compile(rf".*((?=\.tab3)|(?=\.ptm)|(?=\.chemtab)|(?={VERSION_RE}))")
+    ZIP_DIR_PATTERN = re.compile(rf"^(?P<prefix>BIOGRID-.*?)-{VERSION_RE}(\.(tab3|chemtab|ptm|tab))?\.zip$")
     return ZIP_DIR_PATTERN
 
 
 ZIP_DIR_PATTERN = create_zip_re_pattern()
-FILE_PATTERN = re.compile(r"(?P<filename>BIOGRID-[A-Z]+)-*(?P<sub_project>[A-Za-z_]*)-{1}(?P<version_no>[0-9][1-9.]*)")
+
 
 def unzip_file(f):
     file_path = Path(f)
-    match = re.search(ZIP_DIR_PATTERN, file_path.stem)
+    match = re.search(ZIP_DIR_PATTERN, file_path.name)
     print(match)
     if match is not None:
-        directory_base = match[0]
-        filename = re.search(FILE_PATTERN, directory_base)
-        if filename is not None:
-            groups = filename.groupdict()
-            backup = defaultdict(lambda: "")
-            backup |= groups
-            unzip_path = Path("unzip", backup['filename'] + backup['sub_project'])
-            logger.info("unzipping file at %s", unzip_path)
-            with open(file_path, "rb") as zip:
-                try:
-                    zip_root = zipfile.ZipFile(zip)
-                    zip_root.extractall(unzip_path)
-                except zipfile.BadZipFile:
-                    logger.debug("Problem unzipping file", exc_info=1)
-
-
+        directory_base = match.groupdict()['prefix']
+        unzip_path = Path("unzip", directory_base)
+        logger.info("unzipping file at %s", unzip_path)
+        with open(file_path, "rb") as zip:
+            try:
+                zip_root = zipfile.ZipFile(zip)
+                zip_root.extractall(unzip_path)
+            except zipfile.BadZipFile:
+                logger.debug("Problem unzipping file", exc_info=1)
                     
+
 def run_script():
     create_unzip_dir()
     raw_path = Path("download")
